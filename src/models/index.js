6fafb4d0b7abed324493f037c1907d69a69e53fe
Gmati Ayoub <gmtayoyoub15@gmail.com>
2026-08-09 18:39:06 +0200

    modification de index.js ajout de certains model cmmerce produit categorie et les association qui lui correspond

--- src/models/index.js
+++ src/models/index.js
@@ -27,6 +27,9 @@ db.Client = require('./Client')(sequelize, DataTypes);
 db.Commercant = require('./Commercant')(sequelize, DataTypes);
 db.Administrateur = require('./Administrateur')(sequelize, DataTypes);
 db.LogAdmin = require('./LogAdmin')(sequelize, DataTypes);
+db.Commerce = require('./Commerce')(sequelize, DataTypes);
+db.Produit = require('./Produit')(sequelize, DataTypes);
+db.Categorie = require('./Categorie')(sequelize, DataTypes);
 
 // --- Associations (héritage par table, cf. diagramme de classes) ---
 // Utilisateur 1 -- 1 Client / Commercant / Administrateur
@@ -42,5 +45,16 @@ db.Administrateur.belongsTo(db.Utilisateur, { foreignKey: 'utilisateur_id' });
 // Utilisateur "génère" 0..* LogAdmin
 db.Utilisateur.hasMany(db.LogAdmin, { foreignKey: 'utilisateur_id' });
 db.LogAdmin.belongsTo(db.Utilisateur, { foreignKey: 'utilisateur_id' });
+// Utilisateur (commerçant) 1 -- 1 Commerce
+db.Utilisateur.hasOne(db.Commerce, { foreignKey: 'utilisateur_id', onDelete: 'CASCADE' });
+db.Commerce.belongsTo(db.Utilisateur, { foreignKey: 'utilisateur_id' });
+
+// Commerce 1 -- 0..* Produit ("propose")
+db.Commerce.hasMany(db.Produit, { foreignKey: 'commerce_id', onDelete: 'CASCADE' });
+db.Produit.belongsTo(db.Commerce, { foreignKey: 'commerce_id' });
+
+// Categorie 1 -- 0..* Produit ("appartient à")
+db.Categorie.hasMany(db.Produit, { foreignKey: 'categorie_id' });
+db.Produit.belongsTo(db.Categorie, { foreignKey: 'categorie_id' });
 
 module.exports = db;
